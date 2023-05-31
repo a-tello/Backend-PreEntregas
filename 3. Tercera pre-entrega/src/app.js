5dbe25825f6597eaa7c9b41b2e38b4633f7ef547
a-tello <alee.e.tello@gmail.com>
2023-05-31 10:31:04 -0300

    added server settings

--- 3. Tercera pre-entrega/src/app.js
+++ 3. Tercera pre-entrega/src/app.js
@@ -1,9 +1,13 @@
 import express from "express"
+import config from "./config.js"
+import './DAL/mongoDB/dbConfig.js'
 
-const PORT = 8080
+const PORT = config.port
 const app = express()
 
-
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+     
 app.get('/', () => console.log('a'))    
 
 app.listen(PORT,() => console.log(`Listen on port ${PORT}`))
