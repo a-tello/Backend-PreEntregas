67c958dd96f59b456676d31163ec1146f5bc5cf0
a-tello <alee.e.tello@gmail.com>
2023-08-02 13:52:43 -0300

    added tests

--- Refactoring/test/integradores.test.js
+++ Refactoring/test/integradores.test.js
@@ -1,7 +1,12 @@
 import supertest from 'supertest'
 import { expect } from 'chai'
+import config from '../src/config.js'
+
 
 const request = supertest('http://localhost:8080')
+
+let adminToken ='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiQWRtaW4iLCJpYXQiOjE2OTAzODQ4MTgsImV4cCI6MTY5MDM4ODQxOH0.QSXs1-Bx1dkLEnverbDN7RBQZy1MDonEm7K0M3YbvoA'
+let userToken 
 
 const product1 = {
     title: "TestProduct1",
@@ -26,42 +31,152 @@ const product2 = {
     status: true,
     owner:""
 }
+/* 
+describe('Tests de endpoints de Sessions', () => {
 
-describe('Tests de endpoints de Products', () => {
-    /* it('Test metodo POST /api/products', async () => {
-        const response = await request.post('/api/products').send(product1)
-        expect(response._body).to.have.property('_id')
-    }) */
+    it('Test metodo POST /api/sessions/login. Login de admin', async () => {
+        const response = await request.post('/api/sessions/login').send({email: config.admin_email, password: config.admin_password})
+        const tknString = response.headers['set-cookie'][0].split('; ')[0]
+        console.log(tknString);
+        adminToken = tknString.split('=')[1]
+        expect(tknString.split('=')[0]).is.equal('Authorization')
+    })
     
-    it('Test metodo GET sin filtros /api/products', async () => {
+    it('Test metodo POST /api/sessions/login. Login de User/Premium', async () => {
+        const response = await request.post('/api/sessions/login').send({email: 'test@test.com', password: 'test'})
+        const tknString = response.headers['set-cookie'][0].split('; ')[0]
+        userToken = tknString.split('=')[1]
+        expect(tknString.split('=')[0]).is.equal('Authorization')
+    })
+    
+    
+})
+ */
+
+/* describe('Tests de endpoints de Products', () => {
+
+    let id
+
+    it('Test metodo POST /api/products. Agregar producto', async () => {
+        const response = await request.post('/api/products').send(product1).set({ "Authorization": `Bearer ${adminToken}` })
+
+        expect(response._body).to.have.property('_id')
+        expect(response._body.code).that.is.equal(product1.code)
+        id = response._body._id.toString()
+    })
+    
+    it('Test metodo GET sin filtros /api/products. Buscar todos los productos', async () => {
         const response = await request.get('/api/products')
+       
         expect(response._body).to.have.property('status').that.is.equal('success')
         expect(response._body.payload).to.be.an('array').that.is.not.empty
     })
 
-    it('Test metodo GET /api/products/id', async () => {
-        //const createProduct = await request.post('/api/products').send(product2)
-        //console.log(createProduct);
-        //const id = createProduct._body._id
-        const response = await request.get(`/api/products/64b17b9f786c6e16c20837bb`)
-        console.log(response);
-        expect(response._body).to.be.equal({...product2, _id: '64b17b9f786c6e16c20837bb', __v: 0})
+    it('Test metodo GET con filtros /api/products. Buscar todos los productos con filtros', async () => {
+        const response = await request.get('/api/products').query({limit: 5, page: 2})
+        
+        expect(response._body).to.have.property('status').that.is.equal('success')
+        expect(response._body).to.have.property('page').that.is.equal(2)
+        expect(response._body.payload).to.be.an('array').that.is.not.empty
+        expect(response._body.payload).to.be.an('array').to.have.length(5)
     })
-    /*it('Test metodo POST /api/products', async () => {
-        const response = await request.post('/api/products').send(product1)
-        expect(response.body).to.have.property('_id')
-    })
-    it('Test metodo POST /api/products', async () => {
-        const response = await request.post('/api/products').send(product1)
-        expect(response.body).to.have.property('_id')
-    })
-    it('Test metodo POST /api/products', async () => {
-        const response = await request.post('/api/products').send(product1)
-        expect(response.body).to.have.property('_id')
-    })
-    it('Test metodo POST /api/products', async () => {
-        const response = await request.post('/api/products').send(product1)
-        expect(response.body).to.have.property('_id')
-    }) */
 
-})  
+   it('Test metodo GET /api/products/id. Buscar producto por ID', async () => {
+        const response = await request.get(`/api/products/${id}`)
+       
+        expect(response._body).to.have.property('_id').that.is.equal(id)
+    })
+
+    it('Test metodo PUT /api/products/id. Modificar producto', async () => {
+        const response = await request.put(`/api/products/${id}`).send({title: "Modified", stock: 100, category: "ModifiedCategory"}).set({ "Authorization": `Bearer ${adminToken}` })
+
+        expect(response._body).to.have.property('title').that.is.equal('Modified')
+        expect(response._body).to.have.property('stock').that.is.equal(100)
+        expect(response._body).to.have.property('category').that.is.equal('ModifiedCategory')
+    })
+
+    it('Test metodo DELETE /api/products/id. Borrar producto', async () => {
+        await request.delete(`/api/products/${id}`)
+        const response = await request.get(`/api/products/${id}`)
+        
+        expect(response._body).to.be.null
+        
+    })
+})   */
+
+describe('Tests de endpoints de Carts', () => {
+
+    const cartIdTest = '64b55eda5b648c5ef2d69e4e'
+    const productId1 = '643c8ef8fb29b113057e7822'
+    const productId2 = '643c8ef1fb29b113057e7820'
+
+    it('Test metodo POST /api/carts. Crear carrito vacio', async () => {
+        const response = await request.post('/api/carts')
+        expect(response._body.cart).to.have.property('_id')
+        expect(response._body.cart).to.have.property('products')
+    })
+    
+    it('Test metodo POST /api/carts/cartID/products/productID. Agregar productos al carrito (de a uno)', async () => {
+        await request.post(`/api/carts/${cartIdTest}/products/${productId1}`).set({ "Authorization": `Bearer ${userToken}` })
+        await request.post(`/api/carts/${cartIdTest}/products/${productId1}`).set({ "Authorization": `Bearer ${userToken}` })
+        const response = await request.post(`/api/carts/${cartIdTest}/products/${productId2}`).set({ "Authorization": `Bearer ${userToken}` })
+        expect(response._body.cart).to.have.property('_id').that.is.equal(cartIdTest)
+        expect(response._body.cart).to.have.property('products').to.be.length(2)
+        expect(response._body.cart.products[0]).to.have.property('product').to.be.equal(productId1)
+        expect(response._body.cart.products[0]).to.have.property('quantity').to.be.equal(2)
+        expect(response._body.cart.products[1]).to.have.property('product').to.be.equal(productId2)
+        expect(response._body.cart.products[1]).to.have.property('quantity').to.be.equal(1)
+    })
+    
+    it('Test metodo GET  /api/carts/id. Buscar carrito por ID.', async () => {
+        const response = await request.get(`/api/carts/${cartIdTest}`)
+        expect(response._body.cart).to.have.property('_id').that.is.equal(cartIdTest)
+        expect(response._body.cart).to.have.property('products').to.be.an('array').to.be.length(2)
+    })
+
+    it('Test metodo PUT /api/carts/id. Agregar productos por array', async () => {
+        const response = await request.put(`/api/carts/${cartIdTest}`).send([{product:'643c8eecfb29b113057e781e',
+            quantity: 3}, 
+         {product:'643c8ee6fb29b113057e781c',
+            quantity: 5}])
+        expect(response._body.cart).to.have.property('_id').that.is.equal(cartIdTest)
+        expect(response._body.cart).to.have.property('products').to.be.length(4)
+        expect(response._body.cart.products[0]).to.have.property('product').to.be.equal(productId1)
+        expect(response._body.cart.products[0]).to.have.property('quantity').to.be.equal(2)
+        expect(response._body.cart.products[1]).to.have.property('product').to.be.equal(productId2)
+        expect(response._body.cart.products[1]).to.have.property('quantity').to.be.equal(1)
+        expect(response._body.cart.products[2]).to.have.property('product').to.be.equal('643c8eecfb29b113057e781e')
+        expect(response._body.cart.products[2]).to.have.property('quantity').to.be.equal(3)
+        expect(response._body.cart.products[3]).to.have.property('product').to.be.equal('643c8ee6fb29b113057e781c')
+        expect(response._body.cart.products[3]).to.have.property('quantity').to.be.equal(5)
+    })
+
+    it('Test metodo PUT /api/carts/cartID/products/productID. Modificar cantidad de un producto agregado', async () => {
+        const response = await request.put(`/api/carts/${cartIdTest}/products/${productId1}`).send({quantity: 3})
+        expect(response._body.cart).to.have.property('_id').that.is.equal(cartIdTest)
+        expect(response._body.cart).to.have.property('products').to.be.length(4)
+        expect(response._body.cart.products[0]).to.have.property('product').to.be.equal(productId1)
+        expect(response._body.cart.products[0]).to.have.property('quantity').to.be.equal(3)
+        
+    })
+
+    it('Test metodo DELETE /api/carts/cartID/products/productID. Eliminar un producto del carrito', async () => {
+        await request.delete(`/api/carts/${cartIdTest}/products/${productId1}`)
+        const response = await request.get(`/api/carts/${cartIdTest}`)
+        expect(response._body.cart).to.have.property('_id').that.is.equal(cartIdTest)
+        expect(response._body.cart).to.have.property('products').to.be.length(3)
+        expect(response._body.cart.products[0].product).to.have.property('_id').to.be.equal(productId2)
+        expect(response._body.cart.products[0]).to.have.property('quantity').to.be.equal(1)
+        expect(response._body.cart.products[1].product).to.have.property('_id').to.be.equal('643c8eecfb29b113057e781e')
+        expect(response._body.cart.products[1]).to.have.property('quantity').to.be.equal(3)
+        expect(response._body.cart.products[2].product).to.have.property('_id').to.be.equal('643c8ee6fb29b113057e781c')
+        expect(response._body.cart.products[2]).to.have.property('quantity').to.be.equal(5)
+    })
+
+
+    it('Test metodo DELETE /api/carts/id. Vaciar carrito', async () => {
+        await request.delete(`/api/carts/${cartIdTest}`)
+        const response = await request.get(`/api/carts/${cartIdTest}`)
+        expect(response._body.cart.products).to.be.an('array').that.is.empty
+    })
+})
--- Refactoring/test/unitarios.test.js
+++ Refactoring/test/unitarios.test.js
@@ -1,8 +1,206 @@
-/* import { expect } from 'mocha'
+import { expect } from 'chai'
+import '../src/DAL/mongoDB/dbConfig.js'
+import { cartManager } from '../src/DAL/DAOs/carts/cartsMongo.js'
+import { productManager } from '../src/DAL/DAOs/products/productsMongo.js'
 
-describe('Test de DAO de productos', () => {
+/* describe('Test de DAO de carts', () => {
 
-    it('Se crea un producto y se retorna', async () =>{
-        const product = 
+    let cartID
+    const product1 = '643c8ee6fb29b113057e781c'
+    const product2 = '643c8eecfb29b113057e781e'
+    const product3 = {
+        product: '643c8f52fb29b113057e782e',
+        quantity: 5
+    }
+    const product4 = {
+        product: '643c8f42fb29b113057e782c',
+        quantity: 10
+    }
+
+    it('Se crea y se retorna un cart vacío', async () =>{
+        const cart = await cartManager.createOne()
+        cartID = cart._id
+
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').that.is.empty
     })
-}) */
+
+    it('Se busca un carrito por ID', async () =>{
+        const cart = await cartManager.getCartById(cartID)
+
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').that.is.empty
+    })
+    
+    it('Se agregan productos distintos al carrito vacío', async () =>{
+        await cartManager.updateCart(cartID, {$push:{"products":{product: product1, quantity:1}}}, {new:true})
+        await cartManager.updateCart(cartID, {$push:{"products":{product: product2, quantity:1}}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(2)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product1)
+        expect(cart.products[0].quantity).that.is.equal(1)
+        expect(cart.products[1].product._id.toString()).that.is.equal(product2)
+        expect(cart.products[1].quantity).that.is.equal(1)
+    })
+    
+    it('Se elimina un producto del carrito', async () =>{
+        await cartManager.updateCart(cartID, {"$pull":{"products":{"product":product2}}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(1)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product1)
+        expect(cart.products[0].quantity).that.is.equal(1)
+
+    })
+    
+    it('Se agregan productos iguales al carrito vacío', async () =>{
+        await cartManager.updateCart({_id:cartID, "products.product":product1},{$inc:{"products.$.quantity":1}}, {new:true})
+        await cartManager.updateCart({_id:cartID, "products.product":product1},{$inc:{"products.$.quantity":1}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(1)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product1)
+        expect(cart.products[0].quantity).that.is.equal(3)
+        
+    })
+
+    it('Se vacía el carrito', async () =>{
+        await cartManager.updateCart(cartID, {"$pull":{"products":{}}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').that.is.empty
+    })
+
+    it('Se agregan varios productos iguales al carrito vacío', async () =>{
+        await cartManager.updateCart({_id:cartID}, {$push:{"products":{$each :[product3]}}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(1)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product3.product)
+        expect(cart.products[0].quantity).that.is.equal(5)
+        
+    })
+
+    it('Se agregan varios productos diferentes al carrito vacío', async () =>{
+        await cartManager.updateCart(cartID, {"$pull":{"products":{}}}, {new:true})
+        await cartManager.updateCart({_id:cartID}, {$push:{"products":{$each :[product3, product4]}}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(2)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product3.product)
+        expect(cart.products[0].quantity).that.is.equal(5)
+        expect(cart.products[1].product._id.toString()).that.is.equal(product4.product)
+        expect(cart.products[1].quantity).that.is.equal(10)
+    })
+
+    it('Se agregan varios productos diferentes al carrito con productos', async () =>{
+        await cartManager.updateCart({_id:cartID, "products.product":product3.product}, {$inc:{"products.$.quantity":product3.quantity}}, {new:true})
+        await cartManager.updateCart({_id:cartID, "products.product":product4.product}, {$inc:{"products.$.quantity":product4.quantity}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(2)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product3.product)
+        expect(cart.products[0].quantity).that.is.equal(10)
+        expect(cart.products[1].product._id.toString()).that.is.equal(product4.product)
+        expect(cart.products[1].quantity).that.is.equal(20)
+    })
+
+    it('Se modifica la cantidad de un producto dentro de un carrito', async () =>{
+        await cartManager.updateCart({_id:cartID, "products.product": product3.product}, {$set:{"products.$.quantity":25}}, {new:true})
+        const cart = await cartManager.getCartById(cartID)
+
+        expect(cart).to.have.property('_id')
+        expect(cart).to.have.property('products').to.be.an('array').to.have.length(2)
+        expect(cart.products[0]).to.have.keys(['product', 'quantity'])
+        expect(cart.products[0].product._id.toString()).that.is.equal(product3.product)
+        expect(cart.products[0].quantity).that.is.equal(25)
+        expect(cart.products[1].product._id.toString()).that.is.equal(product4.product)
+        expect(cart.products[1].quantity).that.is.equal(20)
+    })
+
+    it('Se elimina un carrito', async () =>{
+        await cartManager.deleteOne(cartID)
+        const cart = await cartManager.getCartById(cartID)
+        
+        expect(cart).is.equal(null)
+    })
+    
+})
+ */
+describe('Test DAO products', () => {
+
+    let productID
+    const product1 = {
+        title: "Test2",
+        description: "Test",
+        price: 844,
+        thumbnail: "https://http2.mlstatic.com/D_NQ_NP_625065-MLA43751822948_102020-O.jpg ",
+        code: "133asaaasds",
+        stock: 50,
+        category: "higiene",
+        status: true,
+        owner:""
+    }
+
+    it('Se busca un producto inexistente', async () => {
+        const product = await productManager.getOneById('643c8ee6fb29b113057e123d')
+
+        expect(product).that.is.equal(null)
+    })
+
+    it('Se agrega un producto', async () => {
+        const product = await productManager.createOne(product1)
+        productID = product._id.toString()
+
+        expect(product).to.have.property('_id')
+        expect(product).to.have.property('status').that.is.equal(true)
+        expect(product).to.have.property('code').that.is.equal(product1.code)
+    })
+
+    it('Se busca un producto existente', async () => {
+        const product = await productManager.getOneById(productID)
+
+        expect(product._id.toString()).that.is.equal(productID)
+        expect(product).to.have.property('status').that.is.equal(true)
+        expect(product).to.have.property('code').that.is.equal(product1.code)
+    })
+
+    it('Se buscan todos los productos', async () => {
+        const products = await productManager.getAll()
+        
+        expect(products).to.have.property('docs').to.be.an('array').that.is.not.empty
+    })
+
+    it('Se modifica un producto', async () => {
+        await productManager.updateOne(productID, {title: 'Modified', stock: 1000,category: "cat.modified"})
+        const product = await productManager.getOneById(productID)
+        
+        expect(product._id.toString()).that.is.equal(productID)
+        expect(product).to.have.property('title').that.is.equal('Modified')
+        expect(product).to.have.property('stock').that.is.equal(1000)
+        expect(product).to.have.property('category').that.is.equal('cat.modified')
+        expect(product).to.have.property('code').that.is.equal(product1.code)
+    })
+
+
+    it('Se elimina un producto', async () => {
+        await productManager.deleteOne(productID)
+        const product = await productManager.getOneById(productID)
+
+        expect(product).that.is.equal(null)
+    })
+})
